fb36fc72df8cab263a6d3159ca5911be3469797c
Ajani Stewart <Ajani.Stewart42@myhunter.cuny.edu>
2017-10-08 15:33:23 -0400

    Homework 7

---
+++ 07/how_many.py
@@ -0,0 +1,29 @@
+def freq(n,l):
+	count = 0
+	for i in l:
+		if i == n:
+			count += 1
+	return count
+
+def min(l):
+	m = max(l)
+	for i in l:
+		if i < m:
+			m = i
+	return m
+
+def mode(l):
+	fList = []
+	for i in l:
+		fList.append(freq(i,l))
+
+	mIndex = 0
+	for i in range(len(fList)):
+		if fList[i] > fList[mIndex]:
+			mIndex = i
+	return l[mIndex]
+
+tList = [3,2,2,1,3,4,5,4,3,4,3]
+print(mode(tList))
+print(min(tList))
+print(freq(3,tList))
